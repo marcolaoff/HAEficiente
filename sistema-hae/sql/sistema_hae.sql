8460dcc76b65a321130d7cf92069f0b2a644d4de
marcolaoff <marcolousilva@gmail.com>
2025-05-29 00:20:02 -0300

    banco exportado

--- sistema-hae/sql/sistema_hae.sql
+++ sistema-hae/sql/sistema_hae.sql
@@ -29,16 +29,19 @@ CREATE TABLE `inscricoes` (
   `descricao` text NOT NULL,
   `arquivo_pdf` varchar(255) DEFAULT NULL,
   `status` varchar(20) DEFAULT 'pendente',
+  `comentario` text DEFAULT NULL,
   `comentario_coordenador` text DEFAULT NULL,
-  `data_envio` datetime DEFAULT current_timestamp()
+  `data_envio` datetime DEFAULT current_timestamp(),
+  `observacoes` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `inscricoes`
 --
 
-INSERT INTO `inscricoes` (`id`, `usuario_id`, `edital_id`, `titulo`, `descricao`, `arquivo_pdf`, `status`, `comentario_coordenador`, `data_envio`) VALUES
-(3, 4, 1, '4124', 'dsadsad', '', 'pendente', NULL, '2025-05-28 13:12:42');
+INSERT INTO `inscricoes` (`id`, `usuario_id`, `edital_id`, `titulo`, `descricao`, `arquivo_pdf`, `status`, `comentario`, `comentario_coordenador`, `data_envio`, `observacoes`) VALUES
+(3, 4, 1, '4124', 'dsadsad', '', 'pendente', NULL, NULL, '2025-05-28 13:12:42', NULL),
+(4, 4, 1, '41234124124', '31312312', '', 'parcial', 'dasdasda', NULL, '2025-05-28 20:45:54', NULL);
 
 -- --------------------------------------------------------
 
@@ -97,8 +100,9 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `usuario`, `senha`, `perfil`, `status`, `aprovado`, `telefone`) VALUES
-(3, 'Administrador do Sistema', 'admin@fatec.com', 'admin', '$2y$10$XPkYuHK2iOZCSFO4UWhW8.0ZoxZzPyE65cd1ILq1IBSrxaTRhST26', 'admin', 'ativo', 1, '(19) 99999-9999'),
-(4, 'marcola', 'marcola@gmail.com', 'marcola', '$2y$10$wLO05W1lIJXpBqIBoKCQmObpbWK1tppgtTrU279rltXmC/5ktzjmy', 'professor', 'ativo', 1, '');
+(3, 'Administrador do Sistema', 'admin@fatec.com', 'admin', '$2y$10$YEpO7amcPw4w3/egPZW9I.90jlOPL/ZUoqZnweLbVi1E7dL8dZ1Zm', 'admin', 'ativo', 1, '(19) 99999-9999'),
+(4, 'marcola', 'marcola@gmail.com', 'marcola', '$2y$10$wLO05W1lIJXpBqIBoKCQmObpbWK1tppgtTrU279rltXmC/5ktzjmy', 'professor', 'ativo', 1, ''),
+(5, 'cleitin', 'cleiton@gmail.com', 'cleitin', '$2y$10$jcvO84nwaFOXpOdVqBLEuuvxuDhOx2e6uV8Mc9uSj6pfP.bwFH.iW', 'coordenador', 'ativo', 1, '(12) 31244-21');
 
 --
 -- Índices para tabelas despejadas
@@ -155,7 +159,7 @@ ALTER TABLE `editais`
 -- AUTO_INCREMENT de tabela `inscricoes`
 --
 ALTER TABLE `inscricoes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `propostas`
@@ -173,7 +177,7 @@ ALTER TABLE `relatorios`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restrições para tabelas despejadas
